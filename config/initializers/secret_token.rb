1b6ad469616edc06a957d1cc75ce749f0ca59da5
EvgeniyBespalov <bespalov1299@mail.ru>
2018-04-03 18:43:02 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoreearthMain::Application.config.secret_key_base = 'fad8d4c40410fab1825db68cf2395d5f8be7f5c65df909b4893b238a8f27f733b1e693c5bb7e60f2297571cb48d6692e42022494d47ebb389a37fd5c997164d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CoreearthMain::Application.config.session_store :cookie_store, key: '_coreearth_main_session'
