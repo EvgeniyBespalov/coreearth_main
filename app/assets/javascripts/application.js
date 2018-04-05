ce67df39f441f35240946ce084c140a9b5493d70
EvgeniyBespalov <bespalov1299@mail.ru>
2018-04-05 12:22:40 +0300

    add files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,25 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function revertValue(id) {
+  document.getElementById(id).value = document.getElementById(id).value.split("").reverse().join("");
+}
+
+function sendAjaxRequest(id, value) {
+  var parse_result;
+  $.ajax({
+    type: "GET",
+    url: "/welcome/ajax_method_from_js",
+    dataType: 'json',
+    data: { data: id, value: value },
+    success: function(result) {
+      //alert (result.data);
+      document.getElementById(id).value = result.value;
+      return false;
+    },
+    error: function() {
+      return false
+    }
+  });
+}
